f81a1f10663be8cc8e387deb1163c8e7a063d018
krsj2150 <kundansen1111@gmail.com>
2016-11-08 18:28:29 +0530

    Binary Exponentiation with MOD

--- binary-exponentiation.cpp
+++ binary-exponentiation.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#define MOD 1000000007
 using namespace std;
 
 long long bin_expo(long long a, long long b)
@@ -11,6 +12,18 @@ long long bin_expo(long long a, long long b)
 		b >>= 1;
 	}
 	return result;
+}
+
+long long bin_expo2(long long a, long long b) //With MOD
+{
+	long long result = 1;
+	while(b){
+		if(b&1)
+			result = ( (result%MOD)*(a%MOD) )%MOD;
+		a = ( (a%MOD)*(a%MOD) )%MOD;
+		b >>= 1;
+	}
+	return (result%MOD);
 }
 
 int main()
