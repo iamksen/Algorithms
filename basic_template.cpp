5751b5388900f980091c9c2f13a945bb14b1641f
krsj2150 <kundansen1111@gmail.com>
2016-11-12 23:32:07 +0530

    Basic template used in competitive programming

---
+++ basic_template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+
+#define REP(i, a, b) for(int i = int(a); i <= int(b); i++)
+#define TRvi(c, it)  for(vi::iterator  it = (c).begin(); it != (c).end(); it++ )
+#define TRvii(c, it) for(vii::iterator it = (c).begin(); it != (c).end(); it++ )
+#define TRmsi(c,it)  for(msi::iterator it = (c).begin(); it != (c).end(); it++ )
+
+#define mp make_pair
+#define pb push_back
+#define INF 1000000000 //1e9
+
+int main(int argc, char const *argv[]) {
+	return 0;
+}
